f8149893a4fe77e44575adc53f36b3f2cc107c4c
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2024-06-11 00:19:06 +0700

    feat: Dashboard UI

--- src/components/utils/sidebar/Sidebar.tsx
+++ src/components/utils/sidebar/Sidebar.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 import SidebarLink from "./SidebarLink";
 import { TSidebarLink } from "@/types/general.types";
 import { usePathname } from "next/navigation";
-import { ArrowBigLeft } from "lucide-react";
+import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
 import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Sidebar() {
@@ -14,7 +14,24 @@ export default function Sidebar() {
   const [isHidden, setIsHidden] = useState(false);
   const pathname = usePathname();
   return (
-    <div className='flex flex-col items-center justify-between h-full  p-8'>
+    <div className='relative flex flex-col items-center justify-start h-full p-8'>
+      <div className='absolute top-1/2 right-0 translate-x-6 p-1 bg-gray-600 dark:bg-gray-200 text-slate-200 dark:text-slate-600 rounded-full'>
+        {isHidden ? (
+          <span
+            className='cursor-pointer'
+            onClick={() => setIsHidden(!isHidden)}
+          >
+            <ArrowBigRight className='h-6 w-6 transition-all' />
+          </span>
+        ) : (
+          <span
+            className='cursor-pointer'
+            onClick={() => setIsHidden(!isHidden)}
+          >
+            <ArrowBigLeft className='h-6 w-6 transition-all' />
+          </span>
+        )}
+      </div>
       <div className='flex flex-col items-center gap-5'>
         <div className='flex flex-col transition-all gap-5'>
           {sidebarLinks.map((link: TSidebarLink) => (
@@ -28,9 +45,6 @@ export default function Sidebar() {
             />
           ))}
         </div>
-        <span onClick={() => setIsHidden(!isHidden)}>
-          <ArrowBigLeft className='h-6 w-6' />
-        </span>
       </div>
       <ModeToggle />
     </div>
